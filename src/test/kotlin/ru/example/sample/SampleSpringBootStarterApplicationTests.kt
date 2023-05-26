dc1e3cca08947d167f382607d331f9fc3fef28cb
nik-sar <n.saraev.ms@gmail.com>
2023-05-26 17:11:45 +0400

    Init project

---
+++ src/test/kotlin/ru/example/sample/SampleSpringBootStarterApplicationTests.kt
@@ -0,0 +1,13 @@
+package ru.example.sample
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SampleSpringBootStarterApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
